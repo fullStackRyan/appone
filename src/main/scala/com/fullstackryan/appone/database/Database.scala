6ae1656eb7aff4c51513bb339471b0a2bff61f9f
Ryan <ryan.mcavoy@itv.com>
2021-01-17 13:26:17 +0000

    testing

--- src/main/scala/com/fullstackryan/appone/database/Database.scala
+++ src/main/scala/com/fullstackryan/appone/database/Database.scala
@@ -1,20 +1,22 @@
+
 package com.fullstackryan.appone.database
 
 import cats.effect.{Async, Blocker, Bracket, ContextShift, Resource, Sync}
+import com.fullstackryan.appone.config.DbConfig
 import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
 import doobie.hikari.HikariTransactor
 import doobie.util.ExecutionContexts
 
 object Database {
 
-  def transactor[F[_]: ContextShift: Async]: Resource[F, HikariTransactor[F]] = {
+  def transactor[F[_]: ContextShift: Async](dbConfig: DbConfig): Resource[F, HikariTransactor[F]] = {
     val config = new HikariConfig()
     config.setDriverClassName("org.postgresql.Driver")
 
-    config.setJdbcUrl("jdbc:postgresql://ec2-52-17-53-249.eu-west-1.compute.amazonaws.com:5432/ddtlm7p23o6ils")
-    config.setUsername("bfzsxjhkfqgdtm")
-    config.setPassword("80f283335a2ff9247dffc3b937b90a77f120c8cf8cf9db432c1f7e9cb7b0fcb6")
-    config.setMaximumPoolSize(10)
+    config.setJdbcUrl(dbConfig.url)
+    config.setUsername(dbConfig.username)
+    config.setPassword(dbConfig.password)
+    config.setMaximumPoolSize(dbConfig.poolSize)
 
     val resources: Resource[F, HikariTransactor[F]] = for {
       cachedThreadPool <- ExecutionContexts.cachedThreadPool[F]
--- src/main/scala/com/fullstackryan/appone/server/ApponeServer.scala
+++ src/main/scala/com/fullstackryan/appone/server/ApponeServer.scala
@@ -2,8 +2,7 @@ package com.fullstackryan.appone.server
 
 import cats.effect.{ConcurrentEffect, ContextShift, Sync, Timer}
 import cats.implicits._
-import com.fullstackryan.appone.config.{Config, DbConfig, ServerConfig}
-//import com.fullstackryan.appone.config.{Config, DbConfig, LoadConfig, ServerConfig}
+import com.fullstackryan.appone.config.{Config, DbConfig, LoadConfig, ServerConfig}
 import com.fullstackryan.appone.database.Database
 import com.fullstackryan.appone.repo.{BookSwap, HelloWorld, Jokes}
 import com.fullstackryan.appone.routing.ApponeRoutes
@@ -13,7 +12,7 @@ import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.Logger
-//import pureconfig.generic.auto._
+import pureconfig.generic.auto._
 
 import java.net.URI
 import scala.concurrent.ExecutionContext.global
@@ -21,31 +20,35 @@ import scala.concurrent.ExecutionContext.global
 
 object ApponeServer {
 
-  def initFlyway[F[_] : Sync]: F[Int] = Sync[F].delay {
-    val flyway = Flyway.configure().dataSource("jdbc:postgresql://ec2-52-17-53-249.eu-west-1.compute.amazonaws.com:5432/ddtlm7p23o6ils", "bfzsxjhkfqgdtm", "80f283335a2ff9247dffc3b937b90a77f120c8cf8cf9db432c1f7e9cb7b0fcb6").baselineOnMigrate(true).load()
+  def initFlyway[F[_] : Sync](url: String, username: String, password: String): F[Int] = Sync[F].delay {
+    val flyway = Flyway.configure().dataSource(url, username, password).baselineOnMigrate(true).load()
     println("inside flyway")
     flyway.migrate()
   }
 
   def prodConfig(): Config = {
-        val dbUri = new URI(System.getenv("JDBC_DATABASE_URL"))
-        val username = dbUri.getUserInfo.split(":")(0)
-        val password = dbUri.getUserInfo.split(":")(1)
-        val dbUrl = "jdbc:postgresql://" + dbUri.getHost + dbUri.getPath
+//    val dbUri = new URI(System.getenv("JDBC_DATABASE_URL"))
+//    val username = dbUri.getUserInfo.split(":")(0)
+//    val password = dbUri.getUserInfo.split(":")(1)
+//    val dbUrl = "jdbc:postgresql://" + dbUri.getHost + dbUri.getPath
 
-        Config(ServerConfig(5432, dbUri.getHost), DbConfig(dbUrl, username, password, 10))
+//    val dbUrl = System.getenv("JDBC_DATABASE_URL")
+//    val username = System.getenv("JDBC_DATABASE_USERNAME")
+//    val password = System.getenv("JDBC_DATABASE_PASSWORD")
+
+    Config(ServerConfig(5432, "ec2-52-17-53-249.eu-west-1.compute.amazonaws.com"), DbConfig("ec2-52-17-53-249.eu-west-1.compute.amazonaws.com:5432", "bfzsxjhkfqgdtm", "80f283335a2ff9247dffc3b937b90a77f120c8cf8cf9db432c1f7e9cb7b0fcb6", 10))
   }
 
   def stream[F[_] : ConcurrentEffect : ContextShift : Timer]: Stream[F, Nothing] = {
     for {
       client <- BlazeClientBuilder[F](global).stream
       // below line loads config from application.conf
-//      config <- Stream.eval(LoadConfig[F, Config].load)
+      config <- Stream.eval(LoadConfig[F, Config].load)
       // This is meant to check if DATABASE_URL is dev or prd
-//      isProdConfig = if (config.dbConfig.url.contains("localhost")) config else prodConfig()
+      isProdConfig = if (config.dbConfig.url.contains("localhost")) config else prodConfig()
       // Below line hopefully passes correct prd or dev config into initFlyway to get a connnection
-      _ <- Stream.eval(initFlyway)
-      xa <- Stream.resource(Database.transactor)
+      _ <- Stream.eval(initFlyway(isProdConfig.dbConfig.url, isProdConfig.dbConfig.username, isProdConfig.dbConfig.password))
+      xa <- Stream.resource(Database.transactor(isProdConfig.dbConfig))
       helloWorldAlg = HelloWorld.impl[F]
       jokeAlg = Jokes.impl[F](client)
       bookAlg = BookSwap.buildInstance[F](xa)
