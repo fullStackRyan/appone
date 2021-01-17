2cd7ed00a6e8904f67f7ad735d6a2b0154019a2f
Ryan <ryan.mcavoy@itv.com>
2021-01-17 09:30:01 +0000

    adding way to check prod or dev

--- src/main/scala/com/fullstackryan/appone/server/ApponeServer.scala
+++ src/main/scala/com/fullstackryan/appone/server/ApponeServer.scala
@@ -2,7 +2,7 @@ package com.fullstackryan.appone.server
 
 import cats.effect.{ConcurrentEffect, ContextShift, Sync, Timer}
 import cats.implicits._
-import com.fullstackryan.appone.config.{Config, LoadConfig, ServerConfig}
+import com.fullstackryan.appone.config.{Config, DbConfig, LoadConfig, ServerConfig}
 import com.fullstackryan.appone.database.Database
 import com.fullstackryan.appone.repo.{BookSwap, HelloWorld, Jokes}
 import com.fullstackryan.appone.routing.ApponeRoutes
@@ -18,22 +18,29 @@ import java.net.URI
 import scala.concurrent.ExecutionContext.global
 
 
-
 object ApponeServer {
 
-  def initFlyway[F[_]: Sync](url: String, username: String, password: String): F[Int] = Sync[F].delay {
+  def initFlyway[F[_] : Sync](url: String, username: String, password: String): F[Int] = Sync[F].delay {
     val flyway = Flyway.configure().dataSource(url, username, password).baselineOnMigrate(true).load()
     println("inside flyway")
     flyway.migrate()
   }
 
-  def stream[F[_] : ConcurrentEffect: ContextShift: Timer]: Stream[F, Nothing] = {
+  def prodConfig(): Config = {
+        val dbUri = new URI(System.getenv("DATABASE_URL"))
+        val username = dbUri.getUserInfo.split(":")(0)
+        val password = dbUri.getUserInfo.split(":")(1)
+        val dbUrl = "jdbc:postgresql://" + dbUri.getHost + dbUri.getPath
+
+        Config(ServerConfig(5432, dbUri.getHost), DbConfig(dbUrl, username, password, 10))
+  }
+
+  def stream[F[_] : ConcurrentEffect : ContextShift : Timer]: Stream[F, Nothing] = {
     for {
       client <- BlazeClientBuilder[F](global).stream
-      // below line loads application.conf
       config <- Stream.eval(LoadConfig[F, Config].load)
-      // below line passes config into intiFlyway to make the database connection.
-      _ <- Stream.eval(initFlyway(config.dbConfig.url, config.dbConfig.username, config.dbConfig.password))
+      isProdConfig = if (config.dbConfig.url.contains("localhost")) config else prodConfig()
+      _ <- Stream.eval(initFlyway(isProdConfig.dbConfig.url, isProdConfig.dbConfig.username, isProdConfig.dbConfig.password))
       xa <- Stream.resource(Database.transactor(config.dbConfig))
       helloWorldAlg = HelloWorld.impl[F]
       jokeAlg = Jokes.impl[F](client)
